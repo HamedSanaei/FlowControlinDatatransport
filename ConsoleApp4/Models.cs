8a2fd02063301529733573a37c17bdd292bfc893
Hamed Sanaei <hamedsan76@outlook.com>
2019-07-02 20:40:40 +0430

    Add project files.

---
+++ ConsoleApp4/Models.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace ConsoleApp4
+{
+    
+    class Node
+    {
+        public Node Neighbor { get; set; }
+        public ulong TransmissionRate { get; set; }
+        public ulong RecieveRate { get; set; }
+        public ulong MaxTransmissionRate { get; set; }
+        public ulong MaxRecieveRate { get; set; }
+        public ulong FrameSize { get; set; }
+        public ulong DatatoSend { get; set; }
+        public TimeSpan Tack { get; set; }
+        public bool FrameLost { get; set; }
+
+        private static System.Timers.Timer aTimer;
+        private static Timer GTimer;
+
+        public int FrameNO { get; set; }
+
+        public ulong GetDatatoSend()
+        {
+            return DatatoSend;
+        }
+
+        public void SetDatatoSend(ulong value)
+        {
+            DatatoSend = value;
+        }
+
+        public ulong Distance { get; set; }
+        public ulong SignalSpeed { get; set; }
+
+        public Node(Node neighbor, ulong trasmissonrate, ulong receiverate, ulong maxtransmissionrate, ulong maxreceiverate, ulong datatoSend)
+
+        {
+            Neighbor = neighbor;
+            neighbor.Neighbor = this;
+
+            TransmissionRate = trasmissonrate;
+            RecieveRate = receiverate;
+            MaxTransmissionRate = maxreceiverate;
+            MaxRecieveRate = MaxRecieveRate;
+            DatatoSend = datatoSend;
+            FrameNO = 0;
+            FrameLost = true;
+        }
+
+        public Node()
+        {
+            FrameNO = 0;
+            FrameLost = true;
+        }
+
+        double CalUtlization(TimeSpan tpropagation,TimeSpan tframe )
+        {
+            var U = (tframe.TotalSeconds) / ((2 * tpropagation.TotalSeconds) + tframe.TotalSeconds);
+            return U;
+        }
+
+        internal double CalUtlization()
+        {
+            TimeSpan Tpropagation = new TimeSpan(Convert.ToInt64((Distance / SignalSpeed) * 10000000));
+            TimeSpan Tframe = new TimeSpan(Convert.ToInt64( (Convert.ToDouble(FrameSize)/ Convert.ToDouble( TransmissionRate)) * 10000000));
+            return CalUtlization(Tpropagation, Tframe);
+        }
+
+        internal long CalBitLength()
+        {
+            return Convert.ToInt64(TransmissionRate * (Distance / SignalSpeed));
+        }
+
+
+        internal void StartFrameSending()
+        {
+            
+            
+
+            TimeSpan Tpropagation = new TimeSpan(Convert.ToInt64((Distance / SignalSpeed) * 10000000));
+            TimeSpan Tframe = new TimeSpan(Convert.ToInt64((Convert.ToDouble(FrameSize) / Convert.ToDouble(TransmissionRate)) * 10000000));
+
+            TimeSpan Tackn = Tpropagation + Tpropagation + Tframe;
+            this.Tack = Tackn;
+            
+            aTimer = new System.Timers.Timer(Tack.TotalMilliseconds);
+            aTimer.Elapsed += OnTimedEvent ;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+
+          
+         
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            int framenumber = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(DatatoSend / FrameSize)));
+            if (FrameNO%12 ==0 && FrameLost == true)
+            {
+                Console.WriteLine("packetNo:" + FrameNO + " Sent At:" + DateTime.Now.ToLongTimeString() + " Ack At:" + "No Ack!");
+                FrameLost = false;
+            }
+            else if (FrameNO % 12 == 0 && FrameLost == false)
+            {
+                Console.WriteLine("packetNo:" + FrameNO + " Sent At:" + DateTime.Now.ToLongTimeString() + " Ack At:" + (DateTime.Now + Tack).ToLongTimeString()+"(Second Try!)" );
+                this.FrameNO++;
+                FrameLost = true;
+            }
+            else
+            {
+                Console.WriteLine("packetNo:" + FrameNO + " Sent At:" + DateTime.Now.ToLongTimeString() + " Ack At:" + (DateTime.Now + Tack).ToLongTimeString());
+                this.FrameNO++;
+            }
+
+            
+            if (framenumber <= FrameNO)
+            {
+                aTimer.Enabled = false;
+                aTimer.Stop();
+                Console.WriteLine("sent!");
+            }
+
+        }
+
+
+        internal void StartFrameSendingGobackN()
+        {
+
+        }
+        
+
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Data size (bit):");
+            ulong DatatoSend = Convert.ToUInt64 (Console.ReadLine());
+
+            Console.WriteLine("Enter bit rate (bps):");
+            ulong B = Convert.ToUInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter distance (meter):");
+            ulong Distance = Convert.ToUInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter signal speed (mps):");
+            ulong SignalSpeed = Convert.ToUInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter Frame size (bit):");
+            ulong FrameSize = Convert.ToUInt64(Console.ReadLine());
+
+            Node nodeA = new Node() {FrameSize = FrameSize, TransmissionRate = B , DatatoSend = DatatoSend,Distance=Distance,SignalSpeed=SignalSpeed};
+            Node nodeB = new Node(nodeA,B,B,0,0,DatatoSend);
+
+
+            Console.WriteLine("============================================");
+            Console.WriteLine("Stop&Wait protocol:");
+
+            Console.WriteLine("Utlization is:"+nodeA.CalUtlization());
+            Console.WriteLine("Bit length of link is:" + nodeA.CalBitLength() +"bit");
+
+            nodeA.StartFrameSending();
+
+            
+            Console.ReadLine();
+        }
+    }
+}
